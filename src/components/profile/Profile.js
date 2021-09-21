e524a85b3ca175b36c8ed869f9eaf6e223ba6537
sayanjitdas <sayanjitgit@gmail.com>
2021-09-21 21:08:12 +0530

    upload profile section with image preview done..

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,8 +1,36 @@
+import { useState,useContext, useEffect } from 'react';
 import './profile.css'
 import thumb from '../../assets/thumb.jpg'
+import { userDetails } from '../../apiservices'
+import { GlobalContext } from '../context/context';
 
 export default function Profile() {
-    let profile_pic = null;
+    const [profilePic,setProfilePic] = useState(null)
+    const [uploadPic,setUploadPic] = useState(null)
+    const [firstName,setFirstName] = useState('')
+    const [lastName,setLastName] = useState('')
+    const [bio,setBio] = useState('')
+
+    const globalData = useContext(GlobalContext)
+    
+    useEffect(() => {
+        (async() => {
+            let response = await userDetails(globalData.token.token)
+            if(!response.error){
+                console.log(response)
+                setFirstName(response.first_name)
+                setLastName(response.last_name)
+                setBio(response.bio)
+                setProfilePic(response.profile_pic)
+            }
+        })()
+    },[])
+
+    useEffect(() => {
+        const previewImage = new File([uploadPic,],'profile_pic.jpg')
+        const url = window.URL.createObjectURL(previewImage)
+        setProfilePic(url)
+    },[uploadPic])
     
     const uploadProfilePic = (e) => {
         e.preventDefault()
@@ -10,8 +38,9 @@ export default function Profile() {
         let input = document.createElement('input')
         input.type = 'file'
         input.onchange = () => {
-            profile_pic = input.files[0]
-            console.log(profile_pic)
+            let profile_pic = input.files[0]
+            setUploadPic(profile_pic)
+
         }
         input.click()
     }
@@ -22,29 +51,29 @@ export default function Profile() {
                 <div className="col-md-6">
                 <form>
                     <div className="text-center mt-5">
-                        <img className="img-thumbnail rounded-circle img-fluid" src={thumb} alt="author" />
+                        <img className="img-thumbnail rounded-circle img-fluid" src={profilePic ? profilePic : thumb } alt="author" />
                     </div>
                     <div className="my-3 d-grid col-lg-3 mx-auto">
-                        <button className="btn btn-sm btn-outline-success" onClick={uploadProfilePic}>Upload Profile Pic</button>
+                        <button className="btn btn-sm btn-outline-success" onClick={uploadProfilePic} >Upload Profile Pic</button>
                     </div>
                     <hr />
                     <div className="row">
                         <div className="col-md-6">
                             <div>
                                 <label htmlFor="Firstname" className="form-label text-muted fw-bold">Firstname</label>
-                                <input id ="Firstname" className="form-control" />
+                                <input id ="Firstname" className="form-control" value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
                             </div>
                         </div>
                         <div className="col-md-6">
                             <div>
                                 <label htmlFor="Lastname" className="form-label text-muted fw-bold">Lastname</label>
-                                <input id ="Lastname" className="form-control" />
+                                <input id ="Lastname" className="form-control" value={lastName} onChange={(e) => setLastName(e.target.value)}/>
                             </div>
                         </div>
                     </div>
                     <div className="mt-4">
                         <label htmlFor="Bio" className="form-label text-muted fw-bold">Bio</label>
-                        <textarea className="form-control" rows="10"></textarea>
+                        <textarea className="form-control" rows="10" value={bio} onChange={(e) => setBio(e.target.value)}></textarea>
                     </div>
                     <div className="d-grid col-md-2">
                         <button type="submit" className="btn btn-lg btn-success mt-3"><span className="fw-bold lead">Submit</span></button>
